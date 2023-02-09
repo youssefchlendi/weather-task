a77844398552a5cb2d88de631b0f2d36d006211c
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-09 21:18:31 +0100

    fetch forecast

--- src/weather/services/weather.service.ts
+++ src/weather/services/weather.service.ts
@@ -1,12 +1,22 @@
 import { Geolocation } from "@capacitor/geolocation";
 import { ref } from "vue";
-import { CurrentWeather, DailyWeather, OneWeather } from "../models/weather.models";
+import { CurrentWeather, DailyWeather, FeelsLike, Temperature } from "../models/weather.models";
 
 const apiKey = "b7bed957715ceda237f558f8e9126a44";
 
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}`;
 
 const currentWeather = ref<CurrentWeather>();
+const forecastWeather = ref<DailyWeather[]>();
+
+fetchCurrentWeather();
+fetchForecastWeather();
+
+setInterval(() => {
+	fetchCurrentWeather();
+	fetchForecastWeather();
+}, 1000 * 60 * 2);
 
 export function useWeather() {
 	return {
@@ -14,13 +24,15 @@ export function useWeather() {
 		fetchCurrentWeather,
 		formatTemperature,
 		getWeatherImageUrl,
+		forecastWeather,
+		fetchForecastWeather
 	}
 }
 
 async function fetchCurrentWeather(){
 	console.log("Fetchig weather");
 	const { coords } = await Geolocation.getCurrentPosition();
-	const response = await fetch("https://api.openweathermap.org/data/2.5/weather?appid=b7bed957715ceda237f558f8e9126a44&lat=1&lon=1");
+	const response = await fetch(`${weatherUrl}&lat=${coords.latitude}&lon=${coords.longitude}`);
 	const res = await response.json();
 
 	currentWeather.value = new CurrentWeather(
@@ -35,10 +47,34 @@ async function fetchCurrentWeather(){
 	);
 }
 
-function formatTemperature(value: number, format: 'F' | 'C' = "C") {
-	return `${Math.round(value)}° ${format} `
+async function fetchForecastWeather() {
+	const { coords } = await Geolocation.getCurrentPosition();
+	const response = await fetch(`${forecastUrl}&lat=${coords.latitude}&lon=${coords.longitude}`);
+	const res = await response.json();
+
+	forecastWeather.value = res.list.map((item: any) => {
+		return new DailyWeather(
+			item.dt,
+			new FeelsLike(item.main.feels_like,item.main.feels_like,item.main.feels_like,item.main.feels_like),
+			item.main.humidity,
+			0,
+			0,
+			new Temperature(
+				item.main.temp_max,
+				item.main.temp_min,
+			),
+			item.weather,
+			item.wind.speed,
+		)
+	});
+
+	
 }
 
-function getWeatherImageUrl(iconName: string, size: '2x' | '4x') {
+function formatTemperature(value: number|undefined , format: 'F' | 'C' = "C") {
+	return value?`${Math.round(value)}° ${format} `:'';
+}
+
+function getWeatherImageUrl(iconName: string|undefined = "04d", size: '2x' | '4x') {
 	return `http://openweathermap.org/img/wn/${iconName}@${size}.png`;
 }
