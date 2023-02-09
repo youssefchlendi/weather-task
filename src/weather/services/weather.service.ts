1b26439cd2f2d1e0f7cc4452d833f78fc1b1e21e
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-09 16:52:45 +0100

    update current-weather ui and add new utilities

--- src/weather/services/weather.service.ts
+++ src/weather/services/weather.service.ts
@@ -1,24 +1,44 @@
 import { Geolocation } from "@capacitor/geolocation";
 import { ref } from "vue";
-import { OneWeather } from "../models/weather.models";
+import { CurrentWeather, DailyWeather, OneWeather } from "../models/weather.models";
 
 const apiKey = "b7bed957715ceda237f558f8e9126a44";
 
 const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}`
 
-const weather = ref<OneWeather>();
+const currentWeather = ref<CurrentWeather>();
 
 export function useWeather() {
 	return {
-		weather,
-		fetchWeather,
+		currentWeather,
+		fetchCurrentWeather,
+		formatTemperature,
+		getWeatherImageUrl,
 	}
 }
 
-async function fetchWeather(){
+async function fetchCurrentWeather(){
 	console.log("Fetchig weather");
 	const { coords } = await Geolocation.getCurrentPosition();
-	const response = await fetch(`${weatherUrl}&lat=${coords.latitude}&lon=${coords.longitude}`);
-	weather.value = await response.json();
+	const response = await fetch("https://api.openweathermap.org/data/2.5/weather?appid=b7bed957715ceda237f558f8e9126a44&lat=1&lon=1");
+	const res = await response.json();
 
+	currentWeather.value = new CurrentWeather(
+		res.main.feels_like,
+		res.main.humidiy,
+		res.sys.sunrise,
+		res.sys.sunset,
+		res.main.temp,
+		res.weather,
+		res.wind.speed,
+
+	);
+}
+
+function formatTemperature(value: number, format: 'F' | 'C' = "C") {
+	return `${Math.round(value)}° ${format} `
+}
+
+function getWeatherImageUrl(iconName: string, size: '2x' | '4x') {
+	return `http://openweathermap.org/img/wn/${iconName}@${size}.png`;
 }
--- src/weather/views/current-weather.vue
+++ src/weather/views/current-weather.vue
@@ -1,28 +1,91 @@
 <template>
-	<div>
-		{{ weather }}
-		<button @click="fetchWeather">fetch</button>
-	</div>
+	<ion-card v-if="currentWeather">
+		<div class="weather-background">
+			<img :src="getWeatherImageUrl(currentWeather.weather[0].icon, '4x')" />
+		</div>
+
+		<ion-card-header>
+			<ion-card-subtitle>
+				{{ currentWeather.weather[0].description }}
+			</ion-card-subtitle>
+			<ion-card-title>
+				{{ formatTemperature(currentWeather.temp, 'F') }}
+			</ion-card-title>
+		</ion-card-header>
+
+		<ion-card-content>
+			<ion-grid>
+				<ion-row>
+					<ion-col>
+						Feels like {{ formatTemperature(currentWeather.feels_like) }}
+					</ion-col>
+
+					<ion-col>
+						Wind Speed {{ Math.round(currentWeather.wind_speed) }}
+					</ion-col>
+				</ion-row>
+
+				<ion-row>
+					<ion-col>
+						Sunrise {{
+							new Date(currentWeather.sunrise * 1000).toLocaleTimeString(undefined, {
+								timeStyle:
+									'short'
+							})
+						}}
+					</ion-col>
+
+					<ion-col>
+						Sunset {{
+							new Date(currentWeather.sunset * 1000).toLocaleTimeString(undefined, {
+								timeStyle:
+									'short'
+							})
+						}}
+					</ion-col>
+				</ion-row>
+			</ion-grid>
+		</ion-card-content>
+	</ion-card>
 </template>
 
 <script lang="ts">
+import commonIonicComponents from '@/shared/common-ionic-components';
 import { defineComponent, onMounted } from 'vue'
-import {useWeather} from '../services/weather.service';
+import { useWeather } from '../services/weather.service';
 export default defineComponent({
-	setup () {
-		const { weather, fetchWeather } = useWeather();
+	components: {
+		...commonIonicComponents
+	},
+	setup() {
+		const { currentWeather, fetchCurrentWeather, formatTemperature, getWeatherImageUrl } = useWeather();
 
-		onMounted(fetchWeather);
+		onMounted(fetchCurrentWeather);
 
 
 		return {
-			weather,
-			fetchWeather
+			currentWeather,
+			fetchCurrentWeather,
+			formatTemperature,
+			getWeatherImageUrl
+
 		}
 	}
 })
 </script>
 
 <style scoped>
+ion-card {
+	max-width: 350px;
+}
 
+ion-card img {
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+.weather-background {
+	background-color: skyblue;
+}
 </style>
